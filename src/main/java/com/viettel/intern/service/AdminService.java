6ebe77f2bf4f804b101a2575e3f754fb8c720ea5
Lương Văn Cường (BU03 PTPM) <cuonglv40@viettel.com.vn>
2023-07-13 17:29:26 +0700

    Revert "Merge branch 'feature/vu-get-courses' into 'develop'"
    This reverts merge request !29

--- src/main/java/com/viettel/intern/service/AdminService.java
+++
@@ -1,8 +0,0 @@
-package com.viettel.intern.service;
-
-import com.viettel.intern.entity.base.Course;
-import org.springframework.data.domain.Page;
-
-public interface AdminService {
-    Page<Course> searchAndSortCourses(String keyword, int page, int size, String sortBy, String sortOrder);
-}
