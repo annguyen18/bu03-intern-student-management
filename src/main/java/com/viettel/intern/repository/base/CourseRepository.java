45f53b2c1641b4e70d8d32e14ed8d2daa0afef43
Lương Văn Cường (BU03 PTPM) <cuonglv40@viettel.com.vn>
2023-07-13 10:17:45 +0000

    Feature/vu get courses api for admin

--- src/main/java/com/viettel/intern/repository/base/CourseRepository.java
+++ src/main/java/com/viettel/intern/repository/base/CourseRepository.java
@@ -1,9 +1,12 @@
 package com.viettel.intern.repository.base;
 
 import com.viettel.intern.entity.base.Course;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CourseRepository extends JpaRepository<Course, Long> {
+    Page<Course> findByNameContainingIgnoreCase(String keyword, Pageable pageable);
 }
---
+++ src/main/java/com/viettel/intern/service/impl/AdminServiceImpl.java
@@ -0,0 +1,41 @@
+package com.viettel.intern.service.impl;
+
+import com.viettel.intern.config.locale.Translator;
+import com.viettel.intern.entity.base.Course;
+import com.viettel.intern.exception.BusinessException;
+import com.viettel.intern.repository.base.CourseRepository;
+import com.viettel.intern.service.AdminService;
+import com.viettel.intern.ultis.SecurityUtil;
+import lombok.AllArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class AdminServiceImpl implements AdminService {
+    private final CourseRepository courseRepository;
+
+    /**
+     * find course containing keyword, use pagination and sort
+     * authority: admin/teacher
+     *
+     * @param page: the number of the page that need to be checked out
+     * @param size: size of each page
+     * @param sortBy: sort by which category
+     * @param sortOrder: order asc or desc
+     */
+    @Override
+    public Page<Course> searchAndSortCourses(String keyword, int page, int size, String sortBy, String sortOrder) {
+        if (SecurityUtil.hasCurrentUserAnyOfAuthorities("admin") || SecurityUtil.hasCurrentUserAnyOfAuthorities("teacher")){
+            Sort.Direction direction = sortOrder.equalsIgnoreCase("desc") ? Sort.Direction.DESC : Sort.Direction.ASC;
+            Sort sort = Sort.by(direction, sortBy);
+            Pageable pageable = PageRequest.of(page, size, sort);
+            return courseRepository.findByNameContainingIgnoreCase(keyword, pageable);
+        } else {
+            throw new BusinessException(Translator.toLocale("user.not-authorized"));
+        }
+    }
+}
